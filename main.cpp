211a352427bf44dd9f550cf0b11ab63db93e2589
ola-tunde <chrisafouda@gmail.com>
2022-03-20 11:05:22 +0100

    main prog

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
+#include "headers/Banque.h"
+#include "headers/Client.h"
+#include "headers/Caissier.h"
+#include "headers/FileAttente.h"
 #include "headers/SED.h"
 
 
 using namespace std;
 
 int main(int argc, char **argv){
-    SED.lancer();
+    int duree, nombreCaissiers, tempsService, tempsArrivee;
+    SED simulation;
+    simulation.lancer();
+    cout << "Bienvenue à la simulation de banque" << endl;
+    cout << "Il est " << simulation.heure() << endl;
+    cout << "Durée prévue de la simulation : ";
+    cin >> duree;
+    cout <<" " << endl;
+    cout << "Nombre de caissiers : " ;
+    cin >> nombreCaissiers;
+    cout <<" " << endl;
+    Banque banque(nombreCaissiers, duree);
+
+    cout << "Temps de service de chaque caissier : " ;
+    cin >> tempsService;
+    cout <<" " << endl;
+    Caissier caissier;
+    caissier(tempsService);
+
+    cout << "Temps entre deux arrivées : ";
+    cin >> tempsArrivee;
+    FileAttente fileAttente;
+    fileAttente(tempsArrivee);
+
+    cout << "Fin de la simulation :"  ;
+
+
+
+
+
+    return 0;
 }
 
-
-//TODO refaire le makefile avec ceci comme exemple
-/*
-    POSTE : Courrier.o Lettre.o Colis.o Sac.o poste.o
-
-    poste : $(POSTE)
-        c++ -o poste $(POSTE)
-
-    Courrier.o : Courrier.h
-    Lettre.o : Courrier.h Lettre.h 
-    Colis.o : Courrier.h Colis.h
-    Sac.o : Courrierh Sac.h 
-    poste.o : Courrier.h Lettre.h Colis.h Sac.h 
-*/
